7b0f3ccc9180ce1a86ceca90babccec49db84e75
Ryan Meline <ryan.c.meline@gmail.com>
2026-02-20 18:19:58 -0800

    changed Get reponse from 'Hello World' to the JSON obj

--- server.go
+++ server.go
@@ -1,14 +1,27 @@
 package main
 
-import "github.com/gofiber/fiber/v3"
+import (
+	"github.com/gofiber/fiber/v3"
+	"time"
+)
+
+type Response struct {
+	Message		string		`json:"message"`
+	Timestamp	time.Time	`json:"timestamp"`
+
+}
 
 func main() {
 	app := fiber.New();
-	
+
+	response := Response {
+		Message:	"My name is Ryan Meline",
+		Timestamp:	time.Now(),
+	}
+
 	app.Get("/", func(c fiber.Ctx) error {
-		return c.SendString("Hello world!")
+		return c.JSON(response)
 	})
 
-	app.Listen(":3000")
-
+	app.Listen(":3000");
 }
